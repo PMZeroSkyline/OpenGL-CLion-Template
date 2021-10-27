bd91ef9c8f50e1a793c249208a1213053b542a49
Zero <guzhang0@gmail.com>
2021-10-28 01:33:17 +0800

    add window.h

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,95 +1,31 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-// ================ Props ================
-
+#include "window.h"
 GLFWwindow* window;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// ================ Functions ================
-// window
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void EventStart()
 {
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
-void ProcessInput(GLFWwindow *window)
+void EventTick()
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-GLFWwindow* CreateWindow(const char* title, int width, int height)
-{
-    // window Init
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return nullptr;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    return window;
-}
-// app
-void Start()
-{
-    window = CreateWindow("OpenGL CLion", SCR_WIDTH, SCR_HEIGHT);
-    // glad Init
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-    }
-}
-void End()
-{
-    glfwTerminate();
-}
-// render
-void RenderStart()
-{
-    ProcessInput(window);
-
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    EventTickWindow(window);
 }
-void RenderTick()
+void EventStop()
 {
-
-}
-void RenderEnd()
-{
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-}
-bool IsRendering()
-{
-    return !glfwWindowShouldClose(window);
+    EventCloseWindow();
 }
 // ================ Main ================
 int main()
 {
-    Start();
-
-    while (IsRendering())
+    window = CreateWindow("OpenGL_CLion_Template", SCR_WIDTH, SCR_HEIGHT);
+    EventStart();
+    while (IsRunning(window))
     {
-        RenderStart();
-        RenderTick();
-        RenderEnd();
+        EventTick();
     }
-
-    End();
+    EventStop();
     return 0;
 }
---
+++ Source/window.h
@@ -0,0 +1,62 @@
+//
+// Created by Zero on 2021/10/28.
+//
+#include <glad/glad.h>
+#ifndef MESHBUILDER_WINDOW_H
+#define MESHBUILDER_WINDOW_H
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+void ProcessInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+GLFWwindow* CreateWindow(const char* title, int width, int height)
+{
+    // window Init
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return nullptr;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return nullptr;
+    }
+    return window;
+}
+bool IsRunning(GLFWwindow *window)
+{
+    return !glfwWindowShouldClose(window);
+}
+void EventTickWindow(GLFWwindow *window)
+{
+    ProcessInput(window);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+void EventCloseWindow()
+{
+    glfwTerminate();
+}
+#endif //MESHBUILDER_WINDOW_H
