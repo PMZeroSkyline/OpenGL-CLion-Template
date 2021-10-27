564bb5b274a985defd909a8e7d3311d09de40902
Zero <guzhang0@gmail.com>
2021-10-27 09:34:11 +0800

    update new

---
+++ cmake-build-debug/CMakeFiles/OpenGL_CLion_Template.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Projects/CPP/OpenGL-CLion-Template/ThirdParty/glad/src/glad.c" "D:/Projects/CPP/OpenGL-CLion-Template/cmake-build-debug/CMakeFiles/OpenGL_CLion_Template.dir/ThirdParty/glad/src/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../ThirdParty/glfw/include"
+  "../ThirdParty/glad/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Projects/CPP/OpenGL-CLion-Template/Source/main.cpp" "D:/Projects/CPP/OpenGL-CLion-Template/cmake-build-debug/CMakeFiles/OpenGL_CLion_Template.dir/Source/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ThirdParty/glfw/include"
+  "../ThirdParty/glad/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGL_CLion_Template.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_CLion_Template.dir/Source/main.cpp.obj"
+  "CMakeFiles/OpenGL_CLion_Template.dir/ThirdParty/glad/src/glad.c.obj"
+  "OpenGL_CLion_Template.exe"
+  "OpenGL_CLion_Template.exe.manifest"
+  "OpenGL_CLion_Template.lib"
+  "OpenGL_CLion_Template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_CLion_Template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
